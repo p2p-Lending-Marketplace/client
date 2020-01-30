af45a537a656445d00dc50a8bf12e014b6353736
Alif Taufik <alif.tr.aminuddin@gmail.com>
2020-01-30 11:45:09 +0700

    First home page without user data

--- components/AlertProfileComponent.js
+++ components/AlertProfileComponent.js
@@ -1,75 +1,70 @@
 import React from 'react'
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-} from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import { multiply } from '../assets/icons'
 import colors from '../assets/colors'
 
-const AlertProfileComponent = ({data}) => {
-    // Variables
-    const handleOnPressApply = data.handleOnPressApply
-    return (
+const AlertProfileComponent = ({ data }) => {
+  // Variables
+  const handleOnPressApply = data.handleOnPressApply
+  return (
+    <View
+      style={{
+        justifyContent: 'center',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#FFF',
+        borderRadius: 15,
+        marginVertical: 10,
+        flexDirection: 'row',
+        elevation: 5,
+        marginHorizontal: 10,
+      }}
+    >
       <View
         style={{
+          flex: 1,
           justifyContent: 'center',
-          padding: 10,
-          backgroundColor: '#FFF',
-          borderRadius: 15,
-          marginVertical: 10,
-          flexDirection: 'row',
-          elevation: 5,
-          marginHorizontal: 10,
+          alignItems: 'center',
         }}
       >
-        <View
-          style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <Image
-            source={require('../assets/icons/cry.png')}
-            style={{ width: 50, height: 50 }}
-          />
-        </View>
-        <View
-          style={{
-            flex: 4,
-            margin: 5,
-            justifyContent: 'center',
-            paddingVertical: 10,
-          }}
-        >
-          <Text style={{ textAlign: 'justify' }}>
-            one more step to apply for installments, let's complete your
-            profile.
+        <Image
+          source={require('../assets/icons/cry.png')}
+          style={{ width: 50, height: 50 }}
+        />
+      </View>
+      <View
+        style={{
+          flex: 4,
+          margin: 5,
+          justifyContent: 'center',
+          paddingVertical: 10,
+        }}
+      >
+        <Text style={{ textAlign: 'justify' }}>
+          one more step to apply for installments, let's complete your profile.
+        </Text>
+        <TouchableOpacity onPress={handleOnPressApply}>
+          <Text
+            style={{
+              textAlign: 'left',
+              color: colors.mainBackground,
+              fontWeight: '700',
+            }}
+          >
+            Go To Profile
           </Text>
-          <TouchableOpacity onPress={handleOnPressApply}>
-            <Text
-              style={{
-                textAlign: 'left',
-                color: colors.mainBackground,
-                fontWeight: '700',
-              }}
-            >
-              Go To Profile
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{ flex: 0.3 }}>
+        </TouchableOpacity>
+      </View>
+      {/* <View style={{ flex: 0.3 }}>
           <TouchableOpacity>
             <Image
               source={{ uri: multiply }}
               style={{ width: 20, height: 20 }}
             />
           </TouchableOpacity>
-        </View>
-      </View>
-    )
+        </View> */}
+    </View>
+  )
 }
 
 export default AlertProfileComponent
